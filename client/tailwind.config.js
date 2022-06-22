8486d7fdd8219ddb0682146edecc0ffedb25c400
davidf1000 <davidfauzi29@gmail.com>
2022-06-22 19:29:22 +0700

    favicon and client metadata

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  experimental: {
+    optimizeUniversalDefaults: true,
+  },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.js',
+    './lib/**/*.js',
+    './data/**/*.mdx',
+  ],
+  darkMode: 'class',
   theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      serif: ['"Roboto Slab"', 'serif'],
+      body: ['Roboto', 'sans-serif'],
+    },
     extend: {},
   },
-  plugins: [],
-  content: ["./src/**/*.{js,jsx,ts,tsx,html}", "./public/index.html"],
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
