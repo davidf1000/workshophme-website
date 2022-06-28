ec854ba41120927d818e302af113fc765c0cb8a6
davidf1000 <davidfauzi29@gmail.com>
2022-06-28 18:58:50 +0700

    add searchbar component

---
+++ client/src/components/dashboard/SearchBar.tsx
@@ -0,0 +1,27 @@
+const SearchBar = ({ wordSearch, setWordSearch, placeholder }: SearchBarProps): JSX.Element => {
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setWordSearch(e.target.value);
+    };
+    return (<>
+        <div className="flex flex-col mb-4 mt-6">
+            <input
+                className="w-96 text-lg shadow appearance-none border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:border-ws-orange focus:ring-ws-orange focus:outline-none focus:shadow-outline"
+                id="passwordRetype"
+                type="text"
+                placeholder={placeholder}
+                name="passwordRetype"
+                value={wordSearch}
+                onChange={(e) => onChange(e)}
+            />
+        </div>
+    </>);
+}
+
+
+interface SearchBarProps {
+    wordSearch: string;
+    setWordSearch: Function;
+    placeholder: string;
+}
+
+export default SearchBar;
--- client/src/components/dashboard/articlelist/Article.tsx
+++ client/src/components/dashboard/articlelist/Article.tsx
@@ -1,17 +1,21 @@
+import { useState } from "react";
+import { Article } from "../../article/article.types";
 import AdminFooter from "../AdminFooter";
 import NavAdmin from "../NavAdmin";
+import SearchBar from "../SearchBar";
 import TopCover from "../TopCover";
 
 const ArticleTable = (): JSX.Element => {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [wordSearch, setWordSearch] = useState<string>('');
+
   return (<div className="flex flex-col h-screen justify-between">
     <div className="flex flex-col">
       <NavAdmin selected='pickup' />
       <TopCover title='Article' desc='List of Articles from WS medium' />
     </div>
-    <div className="bg-white h-full">
-      <h1 className="font-sans text-4xl font-bold font-black animate-in slide-in-from-left">
-        Article Page
-      </h1>
+    <div className="bg-white h-full mx-auto">
+      <SearchBar wordSearch={wordSearch} setWordSearch={setWordSearch} placeholder={"Cari Artikel"} />
     </div>
     <AdminFooter />
   </div>);
